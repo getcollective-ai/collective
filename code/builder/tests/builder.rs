20ca5b9e3a33bccba815ba9039575c53636f8d28
Andrew Gazelka <andrew.gazelka@gmail.com>
2023-03-31 18:38:31 -0700

    add more tests

--- code/builder/tests/builder.rs
+++ code/builder/tests/builder.rs
@@ -24,4 +24,27 @@ fn test_builder() {
     };
 
     assert_eq!(request, expected);
+
+    let request = Request::new("example.com");
+    assert_eq!(request, Request {
+        url: "example.com".to_string(),
+        ..Default::default()
+    });
+
+    let request = Request::new("example.com").path("tester");
+    assert_eq!(request, Request {
+        url: "example.com".to_string(),
+        path: Some("tester".to_string()),
+        ..Default::default()
+    });
+
+    let request = Request::new("example.com")
+        .message("hello")
+        .message("goodbye");
+
+    assert_eq!(request, Request {
+        url: "example.com".to_string(),
+        messages: vec!["hello".to_string(), "goodbye".to_string()],
+        ..Default::default()
+    });
 }
