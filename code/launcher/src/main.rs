24c8a8fd09d14502b5edeb25a7cd5e5b736d8303
Andrew Gazelka <andrew.gazelka@gmail.com>
2023-03-31 21:13:51 -0700

    add todos (#22)

--- code/launcher/src/main.rs
+++ code/launcher/src/main.rs
@@ -1,36 +1,27 @@
 #![allow(unused)]
 
 use anyhow::ensure;
+use log::{error, info};
 use openai::{ChatRequest, Msg};
 use tokio::process::Command;
 
 #[tokio::main]
 async fn main() {
     if let Err(e) = run().await {
-        eprintln!("Error: {e}");
+        error!("{e}");
     }
 }
 
 async fn run() -> anyhow::Result<()> {
-    let openai = openai::Client::simple()?;
-
-    let system = Msg::system(
-        "Provide a list of questions (one per line) that need answering to effectively execute \
-         the given prompt.",
-    );
-
-    let user = Msg::user("Create video caption library");
-
-    let request = ChatRequest::from([system, user]).stop_at("\n");
-
-    let res = openai.chat(request).await?;
-
-    println!("{res}");
+    env_logger::init();
+    build().await?;
 
     Ok(())
 }
 
+/// TODO: include dockerfile in the binary
 async fn build() -> anyhow::Result<()> {
+    info!("🔨 building image");
     let mut cmd = Command::new("docker");
 
     cmd.arg("build").arg("-t").arg("auton").arg(".");
@@ -41,37 +32,8 @@ async fn build() -> anyhow::Result<()> {
     Ok(())
 }
 
-// /// run the image `collective`
-// async fn start() -> anyhow::Result<()> {
-//     let mut cmd = Command::new("docker");
-//
-//     cmd.arg("run")
-//         .arg("-it")
-//         .arg("--rm")
-//         .arg("auton")
-//         .stdin(std::process::Stdio::inherit())
-//         .stdout(std::process::Stdio::inherit());
-//
-//     let mut pty = pty_process::Pty::new().unwrap();
-//     pty.resize(pty_process::Size::new(24, 80)).unwrap();
-//
-//     let mut cmd = pty_process::Command::new("docker");
-//
-//     cmd.arg("run").arg("-it").arg("--rm").arg("auton");
-//
-//     nix
-//     let child = cmd.spawn(&pty.pts().unwrap()).unwrap();
-//
-//     let s = String::new();
-//
-//     let (reader, tx) = pty.split();
-//     let reader = BufReader::new(reader);
-//
-//     let mut lines = reader.lines();
-//
-//     while let Some(line) = lines.next_line().await? {
-//         println!("{}", line);
-//     }
-//
-//     Ok(())
-// }
+/// run the image `collective`
+/// TODO: launch executor in the container
+fn start() -> anyhow::Result<()> {
+    todo!()
+}
